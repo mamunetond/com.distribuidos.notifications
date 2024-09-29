dfd467556119b9bdd71805e1d3c98b9d0120afea
mamunetond <47578828+mamunetond@users.noreply.github.com>
2024-09-29 18:28:18 -0500

    Servicio de notificaciones, este servicio envia una notificación al correo electrónico del ciudadano, también envía una notificación via SMS al ciudadano, finalmente el servicio también le notifica al centralizador.

--- src/main/java/com/distribuidos/notifications/services/NotifService.java
+++ src/main/java/com/distribuidos/notifications/services/NotifService.java
@@ -31,7 +31,7 @@ public class NotifService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
-        message.setFrom("mmunetondurango@gmail.com");
+        message.setFrom("mamunetond@eafit.edu.co");
         mailSender.send(message);
     }
 
