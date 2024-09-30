112f6060e501d5e97ca35fdf0a5846971e780d68
Mario Alejandro Muñetón Durango <47578828+mamunetond@users.noreply.github.com>
2024-09-29 22:52:50 -0500

    Update NotifService.java
    Lineas de código descomentadas

--- src/main/java/com/distribuidos/notifications/services/NotifService.java
+++ src/main/java/com/distribuidos/notifications/services/NotifService.java
@@ -1,42 +1,42 @@
 package com.distribuidos.notifications.services;
 
-//@Service
-//public class NotifService {
+@Service
+public class NotifService {
     
-  //  @Autowired
-    //private JavaMailSender mailSender;
+  @Autowired
+  private JavaMailSender mailSender;
 
-    //@Value("${twilio.account.sid}")
-    //private String accountSid;
+  @Value("${twilio.account.sid}")
+  private String accountSid;
 
-    //@Value("${twilio.auth.token}")
-    //private String authToken;
+  @Value("${twilio.auth.token}")
+  private String authToken;
 
-    //@Value("${twilio.phone.number}")
-    //private String fromPhoneNumber;
+  @Value("${twilio.phone.number}")
+  private String fromPhoneNumber;
 
     //Method to send emails
-    //public void sendEmail(String to, String subject, String body) {
-      //  SimpleMailMessage message = new SimpleMailMessage();
-        //message.setTo(to);
-        //message.setSubject(subject);
-        //message.setText(body);
-        //message.setFrom("mmunetondurango@gmail.com");
-        //mailSender.send(message);
-    //}
+    public void sendEmail(String to, String subject, String body) {
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setTo(to);
+      message.setSubject(subject);
+      message.setText(body);
+      message.setFrom("mmunetondurango@gmail.com");
+      mailSender.send(message);
+    }
 
     //Method to initialize Twilio
-    //private void initTwilio(){
-      //  Twilio.init(accountSid, accountSid);
-    //}
+    private void initTwilio(){
+      Twilio.init(accountSid, accountSid);
+    }
 
-    //public void sendSms(String toPhoneNumber, String body) {
-      //  initTwilio();
+    public void sendSms(String toPhoneNumber, String body) {
+      initTwilio();
 
-        //Message message = Message.creator(
-          //  new PhoneNumber(toPhoneNumber),
-            //new PhoneNumber(fromPhoneNumber),
-            //body) //SMS cotent
-            //.create();
-    //}
-//}
+      Message message = Message.creator(
+      new PhoneNumber(toPhoneNumber),
+      new PhoneNumber(fromPhoneNumber),
+      body) //SMS cotent
+      .create();
+    }
+}
