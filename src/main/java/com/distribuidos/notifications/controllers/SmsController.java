93da19f4973de9e3135dd659dcd7bb433ddb5763
Mario Alejandro Muñetón Durango <47578828+mamunetond@users.noreply.github.com>
2024-09-29 22:49:07 -0500

    Update SmsController.java
    Lineas de código descomentadas

--- src/main/java/com/distribuidos/notifications/controllers/SmsController.java
+++ src/main/java/com/distribuidos/notifications/controllers/SmsController.java
@@ -1,18 +1,18 @@
 package com.distribuidos.notifications.controllers;
 
-//@RestController
-//@RequestMapping("/api/notifications")
-//public class SmsController {
+@RestController
+@RequestMapping("/api/notifications")
+public class SmsController {
 
-  //  @Autowired
-    //private NotifService notifService;
+  @Autowired
+  private NotifService notifService;
 
-    //@PostMapping("/send-sms")
-    //public String enviarSms(
-      //      @RequestParam String toPhoneNumber,
-        //    @RequestParam String body) {
-        //notifService.sendSms(toPhoneNumber, body);
-        //return "SMS successfully sent to" + toPhoneNumber;
-    //}
+  @PostMapping("/send-sms")
+  public String enviarSms(
+  @RequestParam String toPhoneNumber,
+  @RequestParam String body) {
+  notifService.sendSms(toPhoneNumber, body);
+  return "SMS successfully sent to" + toPhoneNumber;
+  }
     
-//}
+}
