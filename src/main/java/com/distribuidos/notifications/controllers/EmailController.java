de10513e3da21b2a9dcd72c4b410c49319cb580b
Mario Alejandro Muñetón Durango <47578828+mamunetond@users.noreply.github.com>
2024-09-29 22:47:21 -0500

    Update EmailController.java
    Lineas de código descomentadas

--- src/main/java/com/distribuidos/notifications/controllers/EmailController.java
+++ src/main/java/com/distribuidos/notifications/controllers/EmailController.java
@@ -1,22 +1,22 @@
 package com.distribuidos.notifications.controllers;
 
-//import com.distribuidos.notifications.services.NotifService;
+import com.distribuidos.notifications.services.NotifService;
 
 
-//@RestController
-//@RequestMapping("/api/notifications")
-//public class EmailController {
+@RestController
+@RequestMapping("/api/notifications")
+public class EmailController {
 
-   // @Autowired
-    //private NotifService notifService;
+   @Autowired
+   private NotifService notifService;
 
-    //@PostMapping("/send-email")
-    //public String sendEmail(
-      //      @RequestParam String to,
-        //    @RequestParam String subject,
-          //  @RequestParam String body) {
-            //    notifService.sendEmail(to, subject, body);
-              //  return "Email successfully sent to" + to;
-    //}
-//}
+   @PostMapping("/send-email")
+   public String sendEmail(
+   @RequestParam String to,
+   @RequestParam String subject,
+   @RequestParam String body) {
+   notifService.sendEmail(to, subject, body);
+   return "Email successfully sent to" + to;
+   }
+}
     
